26c5c8736712ee4f5e45781b280b1ef708c80b4c
chendaole <1174250185@qq.com>
2018-07-03 13:49:33 +0800

    添加项目框架， 添加MusicService

--- app/src/main/java/com/chendaole/kotlinmusicplayer/MainActivity.kt
+++ app/src/main/java/com/chendaole/kotlinmusicplayer/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.chendaole.kotlinmusicplayer
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.chendaole.kotlinmusicplayer.controller.service.MusicService
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initEvent()
+    }
+
+    private fun initEvent() {
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        btnStart.setOnClickListener{
+            handleOnClickBtnStart()
+        }
+    }
+
+    private fun handleOnClickBtnStart() {
+        val intent = Intent(this@MainActivity, MusicService::class.java)
+        startService(intent)
     }
 }
